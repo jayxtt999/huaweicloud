e4f71506b210a69411afced8a1e9a9efe4182e2d
Nai’x <jayxtt999@126.com>
2020-08-25 14:03:08 +0800

    新增磁盘相关

--- src/EvsClient.php
+++ src/EvsClient.php
@@ -33,4 +33,72 @@ class EvsClient extends Client
 
     }
 
+    /**
+     * 创建云硬盘
+     * https://support.huaweicloud.com/api-evs/evs_04_2003.html#section3
+     *
+     * @param $projectId 项目ID
+     * @param $zone  指定要创建云硬盘的所属AZ，若指定的AZ不存在，则创建云硬盘失败
+     * @param $volumeType 云硬盘类型。 目前支持“SSD”，“GPSSD”和“SAS”三种。
+     * @param $size 云硬盘大小，单位为GB
+     * @param $name 云硬盘名称
+     * @param $diskCount 批量创云硬盘的个数。如果无该参数，表明只创建1个云硬盘，目前最多支持批量创建100个
+     *
+     * @return array|bool|mixed|void
+     * @author xietaotao
+     */
+    public function createDisks($projectId, $zone, $volumeType, $size, $name, $diskCount)
+    {
+
+        $this->version    = 'v2.1';
+        $this->curlMethod = 'POST';
+        $this->curlParams = $projectId . '/cloudvolumes';
+
+        $this->curlData = [
+
+            'volume'   => [
+                "name"              => $name,
+                "availability_zone" => $zone,
+                "volume_type"       => $volumeType,
+                "size"              => $size,
+                "count"             => $diskCount,
+            ],
+            'bssParam' => [
+                "chargingMode" => "postPaid",
+                "isAutoPay"    => "true",
+            ],
+        ];
+
+        return $this->request();
+
+    }
+
+    /**
+     * 扩容云硬盘
+     * https://support.huaweicloud.com/api-evs/evs_04_2004.html
+     * @param $projectId 项目ID
+     * @param $volumeId 云硬盘ID 扩容后的云硬盘大小，单位为GB。 扩容后的云硬盘容量范围：大于原有云硬盘容量~云硬盘最大容量（数据盘为32768GB；系统盘为1024GB）
+     * @param $newSize
+     *
+     * @return array|bool|mixed|void
+     * @author xietaotao
+     */
+    public function resizeDisk($projectId,$volumeId,$newSize){
+
+        $this->version    = 'v2.1';
+        $this->curlMethod = 'POST';
+        $this->curlParams = $projectId . '/cloudvolumes/'.$volumeId.'/action';
+        $this->curlData = [
+
+            'os-extend'   => [
+                "new_size"              => $newSize,
+            ],
+            'bssParam' => [
+                "chargingMode" => "postPaid",
+                "isAutoPay"    => "true",
+            ],
+        ];
+
+        return $this->request();
+    }
 }
